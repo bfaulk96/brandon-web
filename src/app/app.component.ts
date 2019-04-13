6168a48a70808bd38688aca3c7353d5fce84f90f
Brandon Faulkner <btfaulkner96@gmail.com>
2019-04-13 00:36:38 -0500

    Initial commit. Sight looks pretty nice

---
+++ src/app/app.component.ts
@@ -0,0 +1,49 @@
+import {Component, OnInit} from '@angular/core';
+import * as Particles from "particlesjs/dist/particles.js"
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+  name = 'Brandon Faulkner';
+  termName = 'brandon.faulkner$';
+  bio = {
+    "Degree": "BS Computer Science",
+    "Occupation": "Application Software Developer",
+    "Company": "Centene Corporation",
+    "Experience": "3.5 years"
+  };
+
+  socialMedia = {
+    "LinkedIn": {
+      "name": "Brandon Faulkner",
+      "link": "https://www.linkedin.com/in/brandon-faulkner-10836276/"
+    },
+    "GitHub": {
+      "name": "bfaulk96",
+      "link": "https://github.com/bfaulk96"
+    },
+    "Facebook": {
+      "name": "Brandon Faulkner",
+      "link": "https://www.facebook.com/monstro222"
+    },
+    "Twitter": {
+      "name": "BFaulk96",
+      "link": "https://twitter.com/BFaulk96"
+    },
+  };
+
+  ngOnInit() {
+    Particles.init({
+      selector: '.background',
+      connectParticles: true,
+      color: ['#2ab816', '#797979', '#4186ff']
+    });
+  }
+
+  getKeys(object) {
+    return Object.keys(object);
+  }
+}
